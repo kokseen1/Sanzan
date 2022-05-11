884e633f6a52f73db13d520ad4f7d3d4b6b22c95
kose <53458630+kokseen1@users.noreply.github.com>
2022-05-11 20:16:53 +0800

    Build: Add setup.py and pyproject.toml

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Sanzan",
+    version="0.0.1",
+    packages=["sanzan"],
+    description="Quick and simple video obfuscation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "youtube-dl == 2020.12.2",
+        "opencv-python",
+        "numpy",
+        "vidgear[core]",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sz = sanzan.sz:main",
+        ]
+    },
+)

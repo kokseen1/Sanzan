4b7a29c91f000fdd7151e7f81df9bd49ec0d4ad8
kose <53458630+kokseen1@users.noreply.github.com>
2022-05-11 19:25:08 +0800

    Fix: Improve argparse checking

--- sz.py
+++ sz.py
@@ -1,28 +1,35 @@
 from sanzan import *
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Connect to an Iro server")
-    
+    parser = argparse.ArgumentParser(description="Encrypt or decrypt video files")
+
     ifile_group = parser.add_mutually_exclusive_group(required=True)
     ifile_group.add_argument("-e", "--enc", help="path of file to encrypt", type=str)
     ifile_group.add_argument("-d", "--dec", help="path of file to decrypt", type=str)
-    
-    parser.add_argument("-k", "--key", help="path of key", type=str)
+
+    parser.add_argument("-k", "--key", help="path of keyfile", type=str)
     parser.add_argument("-o", "--out", help="path of output file", type=str)
-    
-    parser.add_argument('-p', action='store_true')
-    parser.add_argument('-s', action='store_true')
+
+    parser.add_argument("-p", action="store_true")
+    parser.add_argument("-s", action="store_true")
 
     args = parser.parse_args()
+
+    if not (args.out or args.p):
+        parser.error("No action specified, add -o or -p")
+
     if args.enc:
         x = Encryptor(args.enc)
         x.gen_key(args.key)
 
     if args.dec:
+        if not args.key:
+            parser.error("Path of keyfile not specified with -k")
+
         x = Decryptor(args.dec)
-        if args.key:
-            x.set_key(args.key)
+        x.set_key(args.key)
 
     if args.out:
         x.set_output(args.out)

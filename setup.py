baf0f387a916d824635a8527485c839d59c338cb
kose <53458630+kokseen1@users.noreply.github.com>
2022-06-30 21:20:47 +0800

    Doc: Reduce gif sizes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Sanzan",
-    version="0.2.2",
+    version="0.2.3",
     packages=["sanzan"],
     description="Quick and simple video and audio obfuscation",
     long_description=long_description,
